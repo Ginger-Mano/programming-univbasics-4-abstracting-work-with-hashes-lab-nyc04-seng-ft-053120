0ae4620614f4d36b687f3c0ac50cc7fe592bd7f3
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-07 13:51:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def my_hash_creator(key, value)
-  hash = {
+  my_hash_creator = {
     :key => value
-  }
-  puts hash# return a hash that includes the key and value parameters passed into this method
+  }# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
